28fd05d9f4205180d56c81b964e9fd7a09dfc9a3
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-07-15 08:20:15 -0500

    prettifying

--- www/templates/weather/weather.js
+++ www/templates/weather/weather.js
@@ -1,7 +1,7 @@
 angular.module('weatherornot.weather', [])
   .controller('WeatherCtrl', function ($scope, $stateParams, $http) {
     $scope.city = $stateParams.city;
-    //$ionicLoading.show();
+    //  $ionicLoading.show();
     $http.get('/api/forecast/' + $stateParams.lat + ',' + $stateParams.lng).success(function (data) {
       $scope.current = data.currently;
       console.log(data);
